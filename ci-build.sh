3d6fcae25a5bbd917c7e4d280c836797c3651521
Dominik Kobinski <dominikkobinski314@gmail.com>
2024-03-03 20:18:21 +0100

    CI: Update script (part 1)

--- ci-build.sh
+++ ci-build.sh
@@ -1,6 +1,9 @@
-sudo apt-get install gcc make git wget unzip zip libc6-dev-amd64 libc6-dev
+sudo apt-get install gcc make git wget unzip zip libc6-dev #libc6-dev-amd64
 
-#wget http://www.codesourcery.com/sgpp/lite/arm/portal/package5353/public/arm-none-eabi/arm-2009q3-68-arm-none-eabi-i686-pc-linux-gnu.tar.bz2
-#tar xvfj arm-2009q3-68-arm-none-eabi-i686-pc-linux-gnu.tar.bz2
+#sudo dpkg --add-architecture i386
+#sudo apt-get update
 
-make htcleo TOOLCHAIN_PREFIX=arm-none-eabi- #FIXME
+wget http://docs.leaflabs.com/static.leaflabs.com/pub/codesourcery/originals/arm-2009q3-68-arm-none-eabi-i686-pc-linux-gnu.tar.bz2
+tar xvfj arm-2009q3-68-arm-none-eabi-i686-pc-linux-gnu.tar.bz2
+
+make htcleo TOOLCHAIN_PREFIX=./arm-2009q3/bin/arm-none-eabi-
