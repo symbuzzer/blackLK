3a6aea6181e9a9bf2e7ced9574d0f67230c2b9ae
Dominik Kobinski <dominikkobinski314@gmail.com>
2024-07-01 16:46:54 +0200

    CI: Build NBH

--- ci-build.sh
+++ ci-build.sh
@@ -7,3 +7,12 @@ wget http://docs.leaflabs.com/static.leaflabs.com/pub/codesourcery/originals/arm
 tar xvfj arm-2009q3-68-arm-none-eabi-i686-pc-linux-gnu.tar.bz2
 
 make htcleo TOOLCHAIN_PREFIX=./arm-2009q3/bin/arm-none-eabi-
+
+cp ./build-htcleo/lk.bin ./bin/
+
+cd bin
+chmod +x yang
+./nbgen os.nb
+./yang -F RUU_signed.nbh -f os.nb -t 0x400 -s 64 -d PB8110000 -c 11111111 -v CLK1.4.0.2 -l WWE
+cd ..
+cp bin/RUU_signed.nbh ./LEOIMG.nbh
